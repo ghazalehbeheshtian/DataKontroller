dd4c3f836c73b7f26bb747104a189891ca08c6fa
Ghazeleh Beheshtian <Ghazeleh Beheshtian@LAPTOP-T0U167JL>
2023-10-08 12:46:34 +0200

    CssStyleUpdate

--- Uppgift4/SingelR/WebApplication-MVC/wwwroot/js/Temprature.js
+++ Uppgift4/SingelR/WebApplication-MVC/wwwroot/js/Temprature.js
@@ -10,9 +10,22 @@
     connection.on("ReceiveTemperature", function (deviceId, encryptedTemperature) {
         const temperature = DecryptTemperature(encryptedTemperature); // Implementera dekrypteringslogik här
         console.log(`${deviceId}: ${temperature}°C`);
+
+        document.getElementById("loading").style.display = "none"; // dölj laddningsanimationen
+        document.getElementById("connectionError").style.display = "none"; // dölj felmeddelandet
+
     });
 
     connection.start();
+     .catch (function(err) {
+    // visa felmeddelandet om anslutningen misslyckas
+    document.getElementById("connectionError").style.display = "block";
+    });
+
+    connection.onclose(function () {
+    // visa felmeddelandet när anslutningen avbryts
+    document.getElementById("connectionError").style.display = "block";
+});
 
     function DecryptTemperature(encryptedTemperature) {
         // Implementera dekrypteringslogik här
