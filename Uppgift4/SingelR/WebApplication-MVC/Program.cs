bc7984db65c28713f882b364caa4775c178c97c7
Ghazeleh Beheshtian <Ghazeleh Beheshtian@LAPTOP-T0U167JL>
2023-10-09 16:21:39 +0200

    SecurityKlart

--- Uppgift4/SingelR/WebApplication-MVC/Program.cs
+++ Uppgift4/SingelR/WebApplication-MVC/Program.cs
@@ -18,10 +18,32 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+
+
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Add("Content-Security-Policy",
+        "default-src 'self' https://localhost:7107 wss://localhost:7107; " +
+        "connect-src 'self' https://localhost:7107 wss://localhost:7107 wss://localhost:44369/WebApplication-MVC/; " +
+        "script-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com; " +
+        "style-src 'self' 'unsafe-inline'; " +
+        "font-src 'self'; " +
+        "img-src 'self'; " +
+        "frame-src 'self'"
+        );
+
+    await next();
+});
+
+
+
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
 
 app.Run();
